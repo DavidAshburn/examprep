7c2e50da09dfbcaebf2f5b77982ae8e86e932325
DavidAshburn <oneinchwebbing@gmail.com>
2022-08-30 22:14:46 -0700

    heroku push

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+  get 'home/exam'
+  get 'home/exam2'
+  get 'home/exam3'
+  get 'home/exam4'
+  get 'home/exam5'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
